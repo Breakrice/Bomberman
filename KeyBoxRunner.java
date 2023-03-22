a64f5457d9e60d5aa258a90a44f484738983a7eb
Breakrice <118017029+Breakrice@users.noreply.github.com>
2023-03-21 22:01:43 -0500

    Update KeyBoxRunner.java

--- KeyBoxRunner.java
+++ KeyBoxRunner.java
@@ -2,25 +2,25 @@ import javax.swing.JFrame;
 
 public class KeyBoxRunner extends JFrame
 {
-	private static final int WIDTH = 2000;
-	private static final int HEIGHT = 1050;
+    private static final int WIDTH = 1565;
+    private static final int HEIGHT = 688;
 
-	public KeyBoxRunner()
-	{
-		super("Key Listener!!");
+    public KeyBoxRunner()
+    {
+        super("Key Listener!!");
 
-		setSize(WIDTH,HEIGHT);
-		
-		getContentPane().add( new KeyBox() ); 
+        setSize(WIDTH,HEIGHT);
+
+        getContentPane().add( new KeyBox() );
 
 
-		setVisible(true);
-	
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+        setVisible(true);
 
-	public static void main( String args[] )
-	{
-		KeyBoxRunner run = new KeyBoxRunner();
-	}
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args)
+    {
+        KeyBoxRunner run = new KeyBoxRunner();
+    }
 }
