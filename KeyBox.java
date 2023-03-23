4f9a463c429083d4c2255c44c4d2cc526b920539
Breakrice <118017029+Breakrice@users.noreply.github.com>
2023-03-23 13:20:59 -0500

    Update KeyBox.java

--- KeyBox.java
+++ KeyBox.java
@@ -23,23 +23,36 @@ public class KeyBox extends Canvas implements KeyListener
 	}
 
     public void paint( Graphics window )
-    {
-        Graphics2D g2 = (Graphics2D) window;
-        Image background = Toolkit.getDefaultToolkit().getImage("background.png");
-        g2.drawImage(background, 0 , 0 , 1565 , 688 , this);
+	{
+		Graphics2D g2 = (Graphics2D) window;
+		Image background = Toolkit.getDefaultToolkit().getImage("background.png");
+		g2.drawImage(background, 0 , 0 , 1565 , 688 , this);
 
-        Image man = Toolkit.getDefaultToolkit().getImage("man_left_step.png");
-        Image block = Toolkit.getDefaultToolkit().getImage("block.jpg");
-        Image brick = Toolkit.getDefaultToolkit().getImage("brick.jpg");
+		Image man = Toolkit.getDefaultToolkit().getImage("man_left_step.png");
+		Image block = Toolkit.getDefaultToolkit().getImage("block.jpg");
+		Image brick = Toolkit.getDefaultToolkit().getImage("brick.jpg");
 
-        for(int i=0;i<1550;i+=50)
-            g2.drawImage(block, i, 0, 50, 50, this);
-        for(int i=0;i<650;i+=50)
-            g2.drawImage(block, 0, i, 50, 50, this);
-        for(int i=50;i<1550;i+=50)
-            g2.drawImage(block, i, 600, 50, 50, this);
-        for(int i=50;i<650;i+=50)
-            g2.drawImage(block, 1500, i, 50, 50, this);
+		String[][] map=new String[13][31];
+		for(int r=0;r<map.length;r++)
+			for(int c=0;c<map[0].length;c++)
+				map[r][c]="empty";
+		//outer boundaries
+		for(int i=0;i<map[0].length;i++) {
+			g2.drawImage(block, i*spriteSize, 0, 50, 50, this);
+			map[0][i]="block";
+		}
+		for(int i=0;i<map.length;i++) {
+			g2.drawImage(block, 0, i*spriteSize, 50, 50, this);
+			map[i][0]="block";
+		}
+		for(int i=1;i<map[0].length;i++) {
+			g2.drawImage(block, i*spriteSize, 12*spriteSize, 50, 50, this);
+			map[12][i]="block";
+		}
+		for(int i=1;i<map.length;i++) {
+			g2.drawImage(block, 30*spriteSize, i*spriteSize, 50, 50, this);
+			map[i][30]="block";
+		}
         for(int i=100;i<1550;i+=100)
             for(int j=100;j<650;j+=100)
                 g2.drawImage(block, i, j, 50, 50, this);
