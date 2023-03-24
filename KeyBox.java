5e9f60ee0064c9fff368796077fe45fe8c3c3993
carson-davis42 <117942455+carson-davis42@users.noreply.github.com>
2023-03-24 13:15:34 -0500

    Add files via upload

---
+++ KeyBox.java
@@ -0,0 +1,130 @@
+import java.awt.*; /*this must be imported to use Graphics */
+import java.awt.event.*;/*must be imported to use KeyEvents */
+import java.awt.event.KeyListener; /*must be imported in the KeyListener*/
+
+// this is only to test if the keys are pressed.
+public class KeyBox extends Canvas implements KeyListener
+{
+    private String keys_typed;
+    private String key;
+    public static TheGuy guy;
+
+
+    //this is the constructor
+    public KeyBox( )
+    {
+        key = "NO KEY PRESSED YET";
+
+        keys_typed = "";
+
+        addKeyListener( this ); /*all keyListeners must have this in the constructor*/
+
+        setFocusable( true );
+    }
+
+
+    public void paint( Graphics window )
+    {
+
+        Graphics2D g2 = (Graphics2D) window;
+        Image background = Toolkit.getDefaultToolkit().getImage("background.png");
+        g2.drawImage(background, 0 , 0 , 1565 , 688 , this);
+
+        Image man = Toolkit.getDefaultToolkit().getImage("man_left_step.png");
+        Image block = Toolkit.getDefaultToolkit().getImage("block.jpg");
+        Image brick = Toolkit.getDefaultToolkit().getImage("brick.jpg");
+
+        for(int i=0;i<1550;i+=50)
+            g2.drawImage(block, i, 0, 50, 50, this);
+        for(int i=0;i<650;i+=50)
+            g2.drawImage(block, 0, i, 50, 50, this);
+        for(int i=50;i<1550;i+=50)
+            g2.drawImage(block, i, 600, 50, 50, this);
+        for(int i=50;i<650;i+=50)
+            g2.drawImage(block, 1500, i, 50, 50, this);
+        for(int i=100;i<1550;i+=100)
+            for(int j=100;j<650;j+=100)
+                g2.drawImage(block, i, j, 50, 50, this);
+        for(int i=50;i<1500;i+=50)
+            g2.drawImage(brick, i, 50, 50, 50, this);
+        g2.drawImage(man, guy.getX(), guy.getY(), 50, 50, this);
+
+
+
+
+        window.setColor(Color.red);
+        window.drawString( "Please type something!", 250, 100 );
+
+        if(keys_typed.length()!=0)
+        {
+            window.setColor(Color.black);
+            window.fillRect(90, 130, keys_typed.length()*7 + 20, 30);
+        }
+
+        window.setColor(Color.green);
+        window.drawString( keys_typed, 100, 150 );
+
+        window.setColor(new Color( 100, 0, 100 ));
+        window.drawString( key, 250, 200 );
+
+    }
+
+    /*KeyListeners must have these 3 methods:
+     * You do not have to have code in any of these, they can be empty.
+     * But they all must be in your KeyListener for it to work.
+     */
+
+    /*1 */
+    public void keyTyped(KeyEvent e)
+    {
+        keys_typed += "" + e.getKeyChar();
+        repaint();
+    }
+
+    /*2*/
+    public void keyPressed(KeyEvent e)
+    {
+        /*KeyEvent key codes: https://docs.oracle.com/javase/7/docs/api/java/awt/event/KeyEvent.html */
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT ) {
+            Main.moveR=true;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_LEFT ) {
+            Main.moveL=true;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_UP ) {
+            Main.moveU=true;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_DOWN ) {
+            Main.moveD=true;
+        }
+        //add another else if to check if another key was pressed..
+        else {
+            key = "Key " + e.getKeyCode() + " is pressed!";
+        }
+        repaint();
+    }
+
+    /*3*/
+    public void keyReleased(KeyEvent e)
+    {
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT ) {
+           Main.moveR=false;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_LEFT ) {
+            Main.moveL=false;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_UP ) {
+            Main.moveU=false;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_DOWN ) {
+            Main.moveD=false;
+        }
+        //add another else if to check if another key was released..
+        else{
+            key = "Key "+ e.getKeyCode() +" was released!";
+        }
+        repaint();
+
+    }
+
+}
