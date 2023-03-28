635ce78607eb2e60d9d9c3f3e0a7ca5205a2644f
carson-davis42 <117942455+carson-davis42@users.noreply.github.com>
2023-03-28 13:07:08 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean moveR=false;
+    public static boolean moveL=false;
+    public static boolean moveU=false;
+    public static boolean moveD=false;
+    public static void main(String[] args) {
+        //Scanner console = new Scanner(System.in);
+        int let=0;
+        KeyBox.guy=new TheGuy(160,155);
+        new KeyBoxRunner();
+        KeyBox.map=new String[13][31];
+        boolean gameStarted=true;
+        while(gameStarted==true){
+            System.out.print("");
+            if(moveR==true) {
+                KeyBox.guy.moveXright();
+                while(moveR==true){
+                    let++;
+                    let--;
+                    System.out.print("");
+                }
+            }
+            if(moveL==true){
+                KeyBox.guy.moveXleft();
+                while(moveL==true){
+                    let++;
+                    let--;
+                    System.out.print("");
+                }
+            }
+            if(moveU==true){
+                KeyBox.guy.moveYup();
+                while(moveU==true){
+                    let++;
+                    let--;
+                    System.out.print("");
+                }
+            }
+            if(moveD==true){
+                KeyBox.guy.moveYdown();
+                while(moveD==true){
+                    let++;
+                    let--;
+                    System.out.print("");
+                }
+            }
+        }
+    }
+}
