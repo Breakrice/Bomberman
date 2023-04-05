922546aca896f3508e0f84d09877a86a19faca75
carson-davis42 <117942455+carson-davis42@users.noreply.github.com>
2023-04-05 13:44:54 -0500

    Add files via upload

---
+++ TheGuy.java
@@ -0,0 +1,54 @@
+public class TheGuy {
+    private static int xpos; //1565
+    private static int ypos; //688
+    public static int xBIGpos; //31
+    public static int yBIGpos; //13
+    public static boolean placeBomb=false;
+
+    public TheGuy(int x,int y, int Bx,int By){
+        xpos=x;
+        ypos=y;
+        xBIGpos=Bx;
+        yBIGpos=By;
+    }
+
+    public int getX() {
+        return xpos;
+    }
+
+    public int getY(){
+        return ypos;
+    }
+
+    public void moveXright(){
+        xpos+=50;
+        xBIGpos++;
+        for(int yeah=0;yeah<Main.bombs.size();yeah++){
+            Main.bombs.get(yeah).sooner();
+        }
+    }
+
+    public void moveXleft(){
+        xpos-=50;
+        xBIGpos--;
+        for(int yeah=0;yeah<Main.bombs.size();yeah++){
+            Main.bombs.get(yeah).sooner();
+        }
+    }
+
+    public void moveYup(){
+        ypos-=50;
+        yBIGpos--;
+        for(int yeah=0;yeah<Main.bombs.size();yeah++){
+            Main.bombs.get(yeah).sooner();
+        }
+    }
+
+    public void moveYdown(){
+        ypos+=50;
+        yBIGpos++;
+        for(int yeah=0;yeah<Main.bombs.size();yeah++){
+            Main.bombs.get(yeah).sooner();
+        }
+    }
+}
