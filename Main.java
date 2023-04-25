97b59b81aec2133c8397d1d2e5ab7cb21769406e
Breakrice <118017029+Breakrice@users.noreply.github.com>
2023-04-25 12:56:53 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,93 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static boolean moveR=false;
+    public static boolean moveL=false;
+    public static boolean moveU=false;
+    public static boolean moveD=false;
+    public static boolean bombPlace=false;
+    public static ArrayList<Bomb> bombs;
+    public static ArrayList<Enemy> enemies;
+    public static int level;
+    public static boolean nlev;
+    public static boolean gameStarted;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        level=1;
+        KeyBox.map=new Map();
+        int let=0;
+        level=1;
+        bombs=new ArrayList<Bomb>();
+        enemies=new ArrayList<Enemy>();
+        KeyBox.guy=new TheGuy(60,53,1,1);
+        gameStarted=true;
+        nlev=false;
+        new KeyBoxRunner();
+        enemies.add(new Enemy(0,0,0,0));
+
+        while(gameStarted){
+            System.out.print("");
+            if(KeyBox.map.whatMap()[TheGuy.yBIGpos][TheGuy.xBIGpos]=='f'){
+                TheGuy.lives--;
+                if(TheGuy.lives<1)
+                    TheGuy.dead =true;
+            }
+            if(KeyBox.map.whatMap()[TheGuy.yBIGpos][TheGuy.xBIGpos]=='h'){
+                if(TheGuy.lives!=5)
+                    TheGuy.lives++;
+            }
+            if(KeyBox.map.whatMap()[TheGuy.yBIGpos][TheGuy.xBIGpos]=='i'){
+
+            }
+            if(KeyBox.map.whatMap()[TheGuy.yBIGpos][TheGuy.xBIGpos]=='t'){
+
+            }
+            if(moveR) {
+                KeyBox.guy.moveXright();
+                while(moveR){
+                    let++;
+                    let--;
+                    System.out.print("");
+                }
+            }
+            if(moveL){
+                KeyBox.guy.moveXleft();
+                while(moveL){
+                    let++;
+                    let--;
+                    System.out.print("");
+                }
+            }
+            if(moveU){
+                KeyBox.guy.moveYup();
+                while(moveU){
+                    let++;
+                    let--;
+                    System.out.print("");
+                }
+            }
+            if(moveD){
+                KeyBox.guy.moveYdown();
+                while(moveD){
+                    let++;
+                    let--;
+                    System.out.print("");
+                }
+            }
+            if(bombPlace){
+                let++;
+                let--;
+                if(bombs.size()<Bomb.bombs)
+                    bombs.add(new Bomb(KeyBox.guy.getX(),KeyBox.guy.getY(),TheGuy.xBIGpos,TheGuy.yBIGpos));
+                while(bombPlace){
+                    let++;
+                    let--;
+                    System.out.print("");
+                }
+            }
+        }
+    }
+}
