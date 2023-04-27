03d1e64885f7920a2b4352793df64b1392a0e335
carson-davis42 <117942455+carson-davis42@users.noreply.github.com>
2023-04-27 13:28:53 -0500

    Add files via upload

---
+++ Background.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+public class Background extends JPanel
+{
+    public void paintComponent( Graphics window )
+    {
+        Graphics2D g2 = (Graphics2D) window;
+        Image img1 = Toolkit.getDefaultToolkit().getImage("background.png"); /*the image cannot be in the SRC folder*/
+        g2.drawImage(img1, 0, 0, 1565, 688, this);
+    }
+}
